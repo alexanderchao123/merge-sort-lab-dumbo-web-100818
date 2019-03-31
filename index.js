dd18af3477d6ed866ea5db8ed3617c4499cd04f1
Alex Chao <chao.alexander.123@gmail.com>
2019-03-31 23:34:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,14 +26,26 @@ function merge(firstHalf, secondHalf){
   return sorted.concat(firstHalf).concat(secondHalf)
 }
 
-function mergeSort(array) {
-  let midPoint = array.length/2
-  let firstHalf = array.slice(0, midPoint)
-  let secondHalf = array.slice(midPoint, array.length)
+// function mergeSort(array) {
+//   let midPoint = array.length/2
+//   let firstHalf = array.slice(0, midPoint)
+//   let secondHalf = array.slice(midPoint, array.length)
   
-  if (array.length < 2) {
-    return array
-  } else {
-    merge(mergeSort(firstHalf), mergeSort(secondHalf))
+//   if (array.length < 2) {
+//     return array
+//   } else {
+//     merge(mergeSort(firstHalf), mergeSort(secondHalf))
+//   }
+// }
+
+ function mergeSort(array){
+    let midpoint = array.length/2
+    let firstHalf = array.slice(0, midpoint)
+    let secondHalf = array.slice(midpoint, array.length)
+ 
+    if(array.length < 2){
+      return array
+    } else {
+      merge(mergeSort(firstHalf), mergeSort(secondHalf))
+    }
   }
-}
